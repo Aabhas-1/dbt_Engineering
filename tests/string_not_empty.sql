2a02d7b9a31111eecfa07e779447e32ff1d08274
adhaka@delphime.com <adhaka@delphime.com>
2025-02-19 10:41:56 +0000

    Fixed Generic tests

--- tests/string_not_empty.sql
+++ tests/string_not_empty.sql
@@ -1,5 +1,8 @@
-{% test string_not_empty(model,column_name)%}
-select {{column_name}}
-from {{model}}
-where Trim({{column_name}}) = ''
+{% test string_not_empty(model, column_name) %}
+
+select {{ column_name }}
+from {{ model }}
+where {{ column_name }} IS NULL 
+   OR TRIM({{ column_name }}) = ''
+
 {% endtest %}
