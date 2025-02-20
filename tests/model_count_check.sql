0dd65d9f514ea20c4ba5c00bf8665986c8176ed8
adhaka@delphime.com <adhaka@delphime.com>
2025-02-20 11:08:51 +0000

    Added jinja based test

---
+++ tests/model_count_check.sql
@@ -0,0 +1,23 @@
+{# Jinja demo on developing testing code #}
+
+{% set expected_counts = {
+    'sales': 10,
+    'Product': 10,
+    'Region': 10,
+    'Reseller': 10,
+    'Salesperson': 10,
+    'SalespersonRegion': 10,
+    'Targets': 10
+} %}
+
+{% for table, expected_val in expected_counts.items() %}
+    select 
+        '{{ table }}' as table_name,
+        (select count(*) from {{ source('dbo', table) }}) as record_count,
+        {{ expected_val }} as expected_count
+    where (select count(*) from {{ source('dbo', table) }}) < {{ expected_val }}
+    
+    {% if not loop.last %}
+    union all
+    {% endif %}
+{% endfor %}
